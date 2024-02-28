0a42cb0aaf42d472dc041e7bb4c865f7a46d49b0
Jaydeepsinh-Devda <djaybhai76@gmail.com>
2024-02-28 11:56:38 +0530

    Formatted Code

--- lib/view/home/screen/home_screen.dart
+++ lib/view/home/screen/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:filter_bloc_demo/model/price_list_view_model.dart';
 import 'package:filter_bloc_demo/view/home/bloc/filter_bloc.dart';
 import 'package:filter_bloc_demo/view/home/bloc/filter_event.dart';
 import 'package:filter_bloc_demo/view/home/bloc/filter_state.dart';
+import 'package:filter_bloc_demo/view/home/widgets/list_view_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -38,47 +39,49 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("Filter Bloc Demo"),
       ),
-      body: SafeArea(
-        child: BlocBuilder<FilterBloc, FilterState>(
-          builder: (context, state) {
-            if (state is FilterGetListState) {
-              categoryList = state.categoryList;
-              priceList = state.priceList;
-              itemList = state.itemList;
-            }
-            if (state is CategoryListChangeState) {
-              categoryList = state.list;
-            }
-            if (state is PriceListChangeState) {
-              priceList = state.list;
-            }
-            if (state is ItemListChangeState) {
-              itemList = state.list;
-            }
-            return CustomScrollView(
-              slivers: [
-                SliverToBoxAdapter(
-                  child: Column(
-                    children: [
-                      _categoryList(),
-                      const SizedBox(height: 20),
-                      _priceListView(),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-                state is FilterLoadingState
-                    ? _loadingIndicator()
-                    : _itemListView()
-              ],
-            );
-          },
-        ),
-      ),
+      body: SafeArea(child: _blocBuilder()),
     );
   }
 
   //! Widget Methods
+
+  Widget _blocBuilder() => BlocBuilder<FilterBloc, FilterState>(
+        builder: (context, state) {
+          if (state is FilterGetListState) {
+            categoryList = state.categoryList;
+            priceList = state.priceList;
+            itemList = state.itemList;
+          }
+          if (state is CategoryListChangeState) {
+            categoryList = state.list;
+          }
+          if (state is PriceListChangeState) {
+            priceList = state.list;
+          }
+          if (state is ItemListChangeState) {
+            itemList = state.list;
+          }
+          return CustomScrollView(
+            slivers: [
+              _filterOptionsCategoryAndPrice(),
+              state is FilterLoadingState
+                  ? _loadingIndicator()
+                  : _itemListView()
+            ],
+          );
+        },
+      );
+
+  Widget _filterOptionsCategoryAndPrice() => SliverToBoxAdapter(
+        child: Column(
+          children: [
+            _categoryList(),
+            const SizedBox(height: 20),
+            _priceListView(),
+            const SizedBox(height: 20),
+          ],
+        ),
+      );
 
   Widget _loadingIndicator() => const SliverToBoxAdapter(
         child: Center(
@@ -101,23 +104,15 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       );
 
-  Widget _categoryCard(int index) => SizedBox(
-        width: 120,
-        child: GestureDetector(
-          onTap: () {
-            _bloc.add(CategoryListSelectEvent(index: index));
-          },
-          child: Card(
-            color: categoryList[index].isSelected
-                ? Colors.greenAccent
-                : Colors.purple[20],
-            child: Center(
-              child: Text(
-                categoryList[index].name,
-              ),
-            ),
-          ),
-        ),
+  Widget _categoryCard(int index) => ListViewCard(
+        onTap: () {
+          _bloc.add(CategoryListSelectEvent(index: index));
+        },
+        cardWidth: 120,
+        color: categoryList[index].isSelected
+            ? Colors.greenAccent
+            : Colors.purple[20],
+        showListProperty: categoryList[index].name,
       );
 
   Widget _priceListView() => SizedBox(
@@ -131,20 +126,14 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       );
 
-  Widget _priceCard(int index) => SizedBox(
-        width: 120,
-        child: GestureDetector(
-          onTap: () {
-            isSelected = index;
-            _bloc.add(PriceListSelectEvent(index: index));
-          },
-          child: Card(
-            color: isSelected == index ? Colors.redAccent : Colors.purple[20],
-            child: Center(
-              child: Text(priceList[index].price),
-            ),
-          ),
-        ),
+  Widget _priceCard(int index) => ListViewCard(
+        onTap: () {
+          isSelected = index;
+          _bloc.add(PriceListSelectEvent(index: index));
+        },
+        cardWidth: 120,
+        color: isSelected == index ? Colors.redAccent : Colors.purple[20],
+        showListProperty: priceList[index].price,
       );
 
   Widget _itemListView() => SliverList.builder(
@@ -156,20 +145,24 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _itemListTile(int index) => Card(
         child: ListTile(
-          leading: IconButton(
-            icon: itemList[index].isFavorite
-                ? const Icon(Icons.favorite)
-                : const Icon(Icons.favorite_border),
-            onPressed: () {
-              _bloc.add(ItemLikedEvent(index: index));
-            },
-          ),
+          leading: _leadingLikeButton(index),
           title: Text(itemList[index].name),
           subtitle: Text(itemList[index].category.name.toUpperCase()),
-          trailing: Text(
-            "Rs. ${itemList[index].price}",
-            style: const TextStyle(color: Colors.black),
-          ),
+          trailing: _trailingItemPrice(index)
         ),
+      );
+
+  Widget _leadingLikeButton(int index) => IconButton(
+        icon: itemList[index].isFavorite
+            ? const Icon(Icons.favorite)
+            : const Icon(Icons.favorite_border),
+        onPressed: () {
+          _bloc.add(ItemLikedEvent(index: index));
+        },
+      );
+
+  Widget _trailingItemPrice(int index) => Text(
+        "Rs. ${itemList[index].price}",
+        style: const TextStyle(color: Colors.black),
       );
 }
--- lib/view/home/widgets/list_view_card.dart
+++ lib/view/home/widgets/list_view_card.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
 class ListViewCard extends StatelessWidget {
-  final List list;
   final VoidCallback onTap;
-  final double cardHeight;
   final double cardWidth;
   final Color? color;
   final String showListProperty;
 
   const ListViewCard({
-    required this.list,
     required this.onTap,
-    required this.cardHeight,
     required this.cardWidth,
     required this.color,
     required this.showListProperty,
@@ -21,27 +17,16 @@ class ListViewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: cardHeight,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: list.length,
-        itemBuilder: (ctx, index) {
-          return _categoryCard(index);
-        },
+      width: cardWidth,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Card(
+          color: color,
+          child: Center(
+            child: Text(showListProperty),
+          ),
+        ),
       ),
     );
   }
-
-  Widget _categoryCard(int index) => SizedBox(
-        width: cardWidth,
-        child: GestureDetector(
-          onTap: onTap,
-          child: Card(
-            color: color,
-            child: Center(
-              child: Text(showListProperty),
-            ),
-          ),
-        ),
-      );
 }
